7be2f099c72fc8a6740d3a84b192af939e26f130
Sebastian Illa <seba.illa.99@gmail.com>
2023-04-11 20:04:46 -0300

    deafio 1

---
+++ desafio.js
@@ -0,0 +1,44 @@
+class ProductManager{
+    constructor(){
+        this.products = [];
+    }
+    
+    getProducts(){
+        return this.products
+    }
+
+    addProduct(title, description, price, thumbnail, stock){
+        const product = {
+            title,
+            description,
+            price,
+            thumbnail,
+            id: this.#nuevoId() + 1,
+            stock
+        };
+        this.products.push(product)
+    }
+
+    #nuevoId(){
+        let maxId = 0;
+        this.products.map ((product)=> {
+            if (product.id > maxId) maxId = product.id;
+        })
+        return maxId
+    }id
+
+    getProductById(idProduct){
+        return this.products.find((product)=> product.id === idProduct)
+    }
+
+}
+
+const productManager = new ProductManager()
+
+productManager.addProduct("Jenga", "Juego de caja", 1000, "thumbnail", 3)
+productManager.addProduct("Mochila", "Mochila", 500, "thumbnail", 10)
+console.log(productManager.getProducts())
+console.log(productManager.getProductById())
+
+
+
